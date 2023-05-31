984514a0c8ad944174b79c406e22a55c1c353d85
Donovan Rojas <rojasdrm03@gmail.com>
2023-05-31 14:14:43 -0500

    add the container in app id

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@
 const url = 'https://platzi-avo.vercel.app/';
 const urlData = 'api/avo';
 
+const appNode = document.querySelector('#app')
+
 
 async function llamada() {
     try {
@@ -16,25 +18,22 @@ async function llamada() {
         const items = [];
 
         data.forEach(item => {
-            // document.body.insertAdjacentHTML('beforeend', `
-            //     <div class="container">
-            //         <img src="${url}${item.image}" alt="${item.name}">
-            //         <h2>${item.name}</h2>
-            //         <p>$${item.price}<p>
-            //     </div>
-            // `);
+            
             const imagen = document.createElement('img');
+            imagen.src = `${url}${item.image}`
             const title = document.createElement('h2');
+            title.textContent = item.name;
             const price = document.createElement('p');
+            price.textContent = item.price;
 
             const container = document.createElement('div');
             container.append(imagen, title, price);
 
             items.push(container);
         });
-        //console.log(...items);
+        
         // 
-        document.body.append(...items);
+        appNode.append(...items);
 
 
     } catch (error) {
@@ -43,3 +42,12 @@ async function llamada() {
 };
 
 llamada();
+
+
+// document.body.insertAdjacentHTML('beforeend', `
+            //     <div class="container">
+            //         <img src="${url}${item.image}" alt="${item.name}">
+            //         <h2>${item.name}</h2>
+            //         <p>$${item.price}<p>
+            //     </div>
+            // `);
