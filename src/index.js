32ed1fde4ad27d80aa49401c7426cd18af6d1bc6
Donovan Rojas <rojasdrm03@gmail.com>
2023-05-31 14:44:19 -0500

    Intl.numberFormat, and change the css with TailWindCss

--- src/index.js
+++ src/index.js
@@ -9,6 +9,20 @@ const urlData = 'api/avo';
 const appNode = document.querySelector('#app')
 
 
+ // Intl
+ // 1.- Format fechas
+ // 2.- Format monedas
+
+ const formatPrice = price => {
+    //API de internacionalizacion
+    const newPrice = new window.Intl.NumberFormat("en-EN", {
+        style: "currency",
+        currency: "USD"
+    }).format(price)
+
+    return newPrice;
+ }
+
 async function llamada() {
     try {
         const response = await fetch(`${url}${urlData}`);
@@ -20,16 +34,33 @@ async function llamada() {
         data.forEach(item => {
             
             const imagen = document.createElement('img');
-            imagen.src = `${url}${item.image}`
+            imagen.src = `${url}${item.image}`;
+            imagen.className = "h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6"//tailwindcss
             const title = document.createElement('h2');
+            title.className = "text-lg"
             title.textContent = item.name;
+            // title.style.fontSize = '3rem';
+            // title.className = 'bigText';
             const price = document.createElement('p');
-            price.textContent = item.price;
+            price.className = "text-gray-600"
+            price.textContent = formatPrice(item.price);
 
-            const container = document.createElement('div');
-            container.append(imagen, title, price);
+            // Creamos un contenedor el título y el precio
+            const priceAndTitle = document.createElement("div")
+            priceAndTitle.className = "text-center md:text-left";
+            priceAndTitle.appendChild(title);
+            priceAndTitle.appendChild(price);
 
-            items.push(container);
+            // Metemos todo dentro de una tarjeta contenedora
+            const card = document.createElement("div");
+            card.className = "md:flex bg-white rounded-lg p-6 hover:bg-gray-300";
+            card.append(imagen, priceAndTitle);
+
+            // Metemos todo dentro del contenedor principal
+            const contenedor = document.createElement("div");
+            contenedor.appendChild(card);
+
+            items.push(contenedor);
         });
         
         // 
