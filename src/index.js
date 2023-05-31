9300837b1f3c997ec0c0732469c947fada7cfe09
Donovan Rojas <rojasdrm03@gmail.com>
2023-05-31 13:13:16 -0500

    fetch information with async and insert elements

--- src/index.js
+++ src/index.js
@@ -3,4 +3,36 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const url = 'https://platzi-avo.vercel.app/';
+const urlData = 'api/avo';
+
+
+async function llamada() {
+    try {
+        const response = await fetch(`${url}${urlData}`);
+        const resJson = await response.json();
+        console.log(resJson);
+        const data = resJson.data;
+        const items = [];
+
+        data.forEach(item => {
+
+            const container = `
+                <div class="container">
+                    <img src="${url}${item.image}" alt="${item.name}">
+                    <h2>${item.name}</h2>
+                    <p>$${item.price}<p>
+                </div>
+            `;
+            items.push(container);
+        });
+        
+        document.body.insertAdjacentHTML('beforeend', items );
+
+
+    } catch (error) {
+        console.log('Ocurrió un error: ', error);
+    }
+};
+
+llamada();
